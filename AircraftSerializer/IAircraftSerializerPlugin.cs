b61874c326d1cf743122d24ef0d3d1b067c30a0f
polycirrus <polycirrus@gmail.com>
2015-05-16 05:31:07 +0300

    Added the missing file

---
+++ AircraftSerializer/IAircraftSerializerPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AircraftSerializer
+{
+    public interface IAircraftSerializerPlugin
+    {
+        string Name { get; }
+        Type AircraftType { get; }
+    }
+}
